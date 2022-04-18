57bd773475f127c975833b213fdb03aed3807475
= <kienthuc4701@gmail.com>
2022-04-18 09:53:59 +0700

    header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   mode: "jit",
   purge: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-coffee": "#C89F94",
+      },
+      gridTemplateColumns: {
+        "3-col": "2/6 3/6 1/6",
+      },
+      fontFamily: {
+        karla: ["Karla", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
